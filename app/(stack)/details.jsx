521445c45dcfb4c9c0c21d4fe9c033e72d05cb55
seyfettinTulum <tulumseyfettin1@gmail.com>
2024-05-24 13:04:10 +0300

    chore: Add react-native-video dependency and update styles in CountShower and details.jsx

--- app/(stack)/details.jsx
+++ app/(stack)/details.jsx
@@ -1,12 +1,13 @@
-import { View, Text, ImageBackground } from "react-native";
-import React from "react";
+import { View, Text, ImageBackground, Dimensions } from "react-native";
+import React, { useRef, useState } from "react";
 import { useLocalSearchParams } from "expo-router";
 import { SafeAreaView } from "react-native-safe-area-context";
 import PressableButton from "../../Components/Button";
-
+import { StyleSheet } from "react-native";
 const details = () => {
   const param = useLocalSearchParams();
   console.log(param);
+
   return (
     <SafeAreaView className="flex-1">
       <ImageBackground
@@ -15,12 +16,19 @@ const details = () => {
         style={{ flex: 1 }}
       >
         <View className="flex-2 my-2 px-3 ">
-          <ImageBackground
-            source={{ uri: param.url }}
-            resizeMode="cover"
-            style={{ flex: 1 }}
-            borderRadius={20}
-          ></ImageBackground>
+          {param.media_type === "video" ? (
+            <Text className="text-2xl text-[#C2C061]  text-center font-extrabold italic py-2">
+              {" "}
+              Does not support video images!{" "}
+            </Text>
+          ) : (
+            <ImageBackground
+              source={{ uri: param.url }}
+              resizeMode="cover"
+              style={{ flex: 1 }}
+              borderRadius={20}
+            ></ImageBackground>
+          )}
         </View>
         <View className="flex-4">
           <Text className="text-2xl text-[#C2C061]  text-center font-extrabold italic py-2">
@@ -40,5 +48,22 @@ const details = () => {
     </SafeAreaView>
   );
 };
+
+const styles = StyleSheet.create({
+  contentContainer: {
+    flex: 1,
+    padding: 10,
+    alignItems: "center",
+    justifyContent: "center",
+    paddingHorizontal: 50,
+  },
+  video: {
+    width: 350,
+    height: 275,
+  },
+  controlsContainer: {
+    padding: 10,
+  },
+});
 
 export default details;
