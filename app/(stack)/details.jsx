3e60492e8c1a9e87f15f2b30658e98e2438a28dd
seyfettinTulum <tulumseyfettin1@gmail.com>
2024-05-25 19:52:10 +0300

    chore: Update react-native dependencies and add copy functionality to details.jsx

--- app/(stack)/details.jsx
+++ app/(stack)/details.jsx
@@ -1,12 +1,40 @@
-import { View, Text, ImageBackground, Dimensions } from "react-native";
+import { View, Text, ImageBackground, TouchableOpacity } from "react-native";
 import React, { useRef, useState } from "react";
 import { useLocalSearchParams } from "expo-router";
 import { SafeAreaView } from "react-native-safe-area-context";
 import PressableButton from "../../Components/Button";
 import { StyleSheet } from "react-native";
+import { useToast } from "react-native-toast-notifications";
+import * as Clipboard from "expo-clipboard";
+import { Video, ResizeMode } from "expo-av";
+
 const details = () => {
+  const toast = useToast();
   const param = useLocalSearchParams();
+  const video = useRef(null);
+  const [status, setStatus] = useState({});
   console.log(param);
+
+  const copyText = async (text) => {
+    try {
+      await Clipboard.setStringAsync(text);
+      toast.show("Copied Text", {
+        type: "success",
+        placement: "bottom",
+        duration: 1500,
+        offset: 30,
+        animationType: "zoom-in",
+      });
+    } catch (e) {
+      toast.show("An error occurred while copying", {
+        type: "danger",
+        placement: "bottom",
+        duration: 1500,
+        offset: 30,
+        animationType: "zoom-in",
+      });
+    }
+  };
 
   return (
     <SafeAreaView className="flex-1">
@@ -37,9 +65,15 @@ const details = () => {
           <Text className="text-md text-[#C2C061]  text-center font-extrabold italic py-1">
             {param.date}
           </Text>
-          <Text className="text-xs text-white text-center font-bold italic py-2">
-            {param.explanation}
-          </Text>
+          <TouchableOpacity
+            onPress={() => {
+              copyText(param.explanation);
+            }}
+          >
+            <Text className="text-xs text-white text-center font-bold italic py-2 px-1 select-text">
+              {param.explanation}
+            </Text>
+          </TouchableOpacity>
           <View className="justify-center items-center">
             <PressableButton href={param.url} title="Download" />
           </View>
